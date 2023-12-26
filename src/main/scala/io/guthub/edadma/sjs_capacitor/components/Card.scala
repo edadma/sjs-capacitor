4b2bc60b95636f9593909b19ee5f0f3799f44054
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-26 00:24:23 -0500

    Card

--- src/main/scala/io/guthub/edadma/sjs_capacitor/components/Card.scala
+++ src/main/scala/io/guthub/edadma/sjs_capacitor/components/Card.scala
@@ -4,6 +4,6 @@ import com.raquo.laminar.api.L.{*, given}
 
 def Card(content: Node*) =
   div(
-    cls := "max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700",
+    cls := "max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700",
     content,
   )
