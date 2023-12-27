a8610e4701854529eb533f9f42d807bc8ee81be7
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-27 15:52:10 -0500

    fetching

--- src/main/scala/io/guthub/edadma/sjs_capacitor/App.scala
+++ src/main/scala/io/guthub/edadma/sjs_capacitor/App.scala
@@ -3,16 +3,22 @@ package io.guthub.edadma.sjs_capacitor
 import com.raquo.laminar.DomApi
 import com.raquo.laminar.api.L.{*, given}
 import components.*
-import io.laminext.fetch._
-import concurrent.ExecutionContext.Implicits.global
+//import io.laminext.fetch._
+//import concurrent.ExecutionContext.Implicits.global
+
+import typings.capacitorCore.mod.CapacitorHttp.get
+import typings.capacitorCore.typesCorePluginsMod.HttpOptions
 
 def App =
   div(
     cls := "max-w-full p-6 bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700",
-    child <-- Fetch
-      .get("text/juan/1")
-      .text
-      .data
+    child <-- EventStream
+      .fromJsPromise(get(HttpOptions("text/juan/1")))
+      .map(response => response.data.asInstanceOf[String])
+//    child <-- Fetch
+//      .get("text/juan/1")
+//      .text
+//      .data
 //    child.text <-- FetchStream
 //      .get("text/juan/1"),
 //      .map(_.responseText)
